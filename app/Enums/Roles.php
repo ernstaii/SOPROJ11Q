2375200da1f4be2e501641f66683a705a45a7a53
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-04-01 17:56:58 +0200

    feat(users): Migration for editing users-table

---
+++ app/Enums/Roles.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class Roles extends Enum
+{
+    const None = 0;
+    const Thief = 1;
+    const UndercoverThief = 2;
+    const Police = 3;
+}
---
+++ database/migrations/2021_04_01_125141_update_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Enums\Roles;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('location', 255)->nullable()->change();
+            $table->string('invite_key')->nullable();
+            $table->tinyInteger('role')->default(Roles::None);
+
+            $table->foreign('invite_key')->references('value')->on('invite_keys');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('location', 255)->change();
+            $table->dropForeign('invite_key');
+            $table->dropColumn('invite_key');
+            $table->dropColumn('role');
+        });
+    }
+}
