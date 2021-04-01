628ab476b27cd596907d08069209597c721facfb
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-04-01 20:25:06 +0200

    fix(users): Updated Unit tests

--- app/Enums/Roles.php
+++ app/Enums/Roles.php
@@ -11,8 +11,8 @@ use BenSampo\Enum\Enum;
  */
 final class Roles extends Enum
 {
-    const None = 0;
-    const Thief = 1;
-    const UndercoverThief = 2;
-    const Police = 3;
+    const None = "0";
+    const Thief = "1";
+    const UndercoverThief = "2";
+    const Police = "3";
 }
