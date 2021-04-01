7d7619891dae930431b263750638d0b42dcad1ef
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-04-01 20:29:36 +0200

    feat(users): Get users in game

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -8,8 +8,8 @@ class GameController extends Controller
 {
     public function getUsersInGame($gameId)
     {
-        return dd(User::query()->whereHas('inviteKey', function ($query) use ($gameId) {
+        return User::query()->whereHas('inviteKey', function ($query) use ($gameId) {
             return $query->where('game_id', $gameId);
-        })->get());
+        })->get();
     }
 }
