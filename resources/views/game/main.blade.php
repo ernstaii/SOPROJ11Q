d5f501dec837f20e2d96b2a02b160e2e47365ba4
LeonNiekus <leonniekusnl@gmail.com>
2021-04-13 14:44:44 +0200

    yeet buttons

--- resources/views/game/main.blade.php
+++ resources/views/game/main.blade.php
@@ -6,4 +6,22 @@
 
 @section('content')
     <h1>Game Screen</h1>
+    <div class="button-2">
+        <form action="/games/{{$id}}" method="post">
+            <div class="form-item">
+                @csrf
+                @method('PUT')
+                <button type="input" class="keys-share-button" type="submit" name="state" value="{{\App\Enums\Statuses::Paused}}}">Pauzeer spel</button>
+            </div>
+        </form>
+    </div>
+    <div class="button-3">
+        <form action="/games/{{$id}}" method="post">
+            <div class="form-item">
+                @csrf
+                @method('PUT')
+                <button type="input" class="keys-share-button" type="submit" name="state" value="{{\App\Enums\Statuses::Ongoing}}}">Hervat spel</button>
+            </div>
+        </form>
+    </div>
 @endsection
