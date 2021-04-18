6eb201f038a2658b84aaf2097fbc1d7d37e3a798
Luke Derkzen <lderkzen@gmail.com>
2021-04-18 19:03:52 +0200

    demo

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\ConfigController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
