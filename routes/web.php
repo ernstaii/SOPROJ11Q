b81878790e1d44334b1df3da47d23f8e9a9fe749
Luke Derkzen <lderkzen@gmail.com>
2021-04-18 19:06:55 +0200

    attempt 2

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\ConfigController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
