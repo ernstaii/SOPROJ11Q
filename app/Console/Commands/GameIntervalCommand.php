df242d549737b996cda4c223bbf5bf99d2e828f0
imfi-jz <qasloot@avans.nl>
2021-04-18 11:28:23 +0200

    Working (draft)

---
+++ app/Console/Commands/GameIntervalCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\Statuses;
+use App\Events\GameIntervalEvent;
+use App\Models\Game;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class GameIntervalCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'game:interval';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Takes care of pushing game interval events';
+    protected $lastUpdates = [];
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        for($i = 0; $i < 60; $i += 5) {
+            echo "Checking intervals\n";
+
+            $now = Carbon::now();
+
+            foreach (Game::all() as $game) {
+                if ($game->status == Statuses::Ongoing) {
+                    if (!array_key_exists($game->id, $this->lastUpdates)) {
+                        $this->lastUpdates[$game->id] = $now;
+                    }
+
+                    $difference = $now->diffInSeconds($this->lastUpdates[$game->id]);
+                    echo "Difference: " . $difference . "\n";
+
+                    if ($difference >= $game->interval) {
+                        event(new GameIntervalEvent($game->id));
+                        $this->lastUpdates[$game->id] = $now;
+                        echo "Invoking interval of game " . $game->id . "\n";
+                    }
+                }
+            }
+
+            sleep(5);
+        }
+        return 0;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('game:interval')
+            ->everyMinute()
+            ->withoutOverlapping()
+            ->runInBackground();
+
+        // If you want debug output in a text file, add this
+            //->sendOutputTo(getcwd() . "\\IntervalOut.txt", true);
     }
 
     /**
