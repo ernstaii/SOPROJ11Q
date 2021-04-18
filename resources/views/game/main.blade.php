f555ca4dd487e2e90bf5179e8404b37d870ac6f2
Jesse Stupers <49638126+Stupers@users.noreply.github.com>
2021-04-18 19:22:48 +0200

    Commiterdemit

--- resources/views/game/main.blade.php
+++ resources/views/game/main.blade.php
@@ -1,17 +1,17 @@
 @extends('layout')
 
 @section('head')
-    <title>Overzicht Game {{$id}}</title>
+    <title>Overzicht Spel {{$id}}</title>
 @endsection
 
 @section('content')
-    <h1>Game Screen</h1>
+    <h1>Spel Scherm</h1>
     <div class="button-1">
         <form action="/games/{{$id}}" method="post">
             <div class="form-item">
                 @csrf
                 @method('PUT')
-                <label for="reason">Reden van stoppen</label>
+                <label for="reason">Geef een reden voor het beëindigen</label>
                 <input type="text" id="reason" name="reason">
                 <button type="input" class="keys-share-button" type="submit" name="state" value="{{\App\Enums\Statuses::Finished}}">Beëindig spel</button>
             </div>
@@ -22,7 +22,7 @@
             <div class="form-item">
                 @csrf
                 @method('PUT')
-                <label for="reason">Reden van pauzeren</label>
+                <label for="reason">Geef een reden voor het pauzeren</label>
                 <input type="text" id="reason" name="reason">
                 <button type="input" class="keys-share-button" type="submit" name="state" value="{{\App\Enums\Statuses::Paused}}">Pauzeer spel</button>
             </div>
