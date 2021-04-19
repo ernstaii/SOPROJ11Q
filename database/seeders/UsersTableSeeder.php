244f04e583cfdc0573bea2ba26609eaab8c6f36b
Luke Derkzen <lderkzen@gmail.com>
2021-04-19 23:30:43 +0200

    random user locations in Oss

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -13,17 +13,23 @@ class UsersTableSeeder extends Seeder
     {
         $id = Game::all()->first()->id;
 
+        $latitude = "51.7" . strval(rand(43866, 79043));
+        $longitude = "5." . strval(rand(491387, 553818));
+
         User::query()->create([
             'username' => 'Willem',
-            'location' => '51.498134,-0.201755',
+            'location' => $latitude . ',' . $longitude,
             'invite_key' => 'A0B3',
             'game_id' => $id,
             'role' => Roles::Thief,
         ]);
 
+        $latitude = "51.7" . strval(rand(43866, 79043));
+        $longitude = "5." . strval(rand(491387, 553818));
+
         User::query()->create([
             'username' => 'Bart',
-            'location' => '51.498134,-0.201755',
+            'location' => $latitude . ',' . $longitude,
             'invite_key' => 'LKJ3',
             'game_id' => $id,
             'role' => Roles::Police,
@@ -31,17 +37,23 @@ class UsersTableSeeder extends Seeder
 
         $id = Game::all()->last()->id;
 
+        $latitude = "51.7" . strval(rand(43866, 79043));
+        $longitude = "5." . strval(rand(491387, 553818));
+
         User::query()->create([
             'username' => 'Johan',
-            'location' => '51.498134,-0.201755',
+            'location' => $latitude . ',' . $longitude,
             'invite_key' => 'BF3V',
             'game_id' => $id,
             'role' => Roles::Police,
         ]);
 
+        $latitude = "51.7" . strval(rand(43866, 79043));
+        $longitude = "5." . strval(rand(491387, 553818));
+
         User::query()->create([
             'username' => 'Rik',
-            'location' => '51.498134,-0.201755',
+            'location' => $latitude . ',' . $longitude,
             'invite_key' => 'FF3Q',
             'game_id' => $id,
             'role' => Roles::Thief,
