7aa9d1d2a36f0db4b37e40892b29067efc2a005a
Luke Derkzen <lderkzen@gmail.com>
2021-04-23 08:41:54 +0200

    ide-helper fix
    Separate file caused warnings.
    Model ide-helpers won't auto-generate anymore when running commands. It has to be done manually.

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -6,6 +6,33 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Game
+ *
+ * @property int $id
+ * @property string $status
+ * @property int $duration
+ * @property int $interval
+ * @property int $time_left
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read Collection|\App\Models\InviteKey[] $invite_keys
+ * @property-read int|null $invite_keys_count
+ * @property-read Collection|\App\Models\Loot[] $loot
+ * @property-read int|null $loot_count
+ * @method static \Database\Factories\GameFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Game newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Game newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Game query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Game whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Game whereDuration($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Game whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Game whereInterval($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Game whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Game whereTimeLeft($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Game whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Game extends Model
 {
     use HasFactory;
--- app/Models/InviteKey.php
+++ app/Models/InviteKey.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\InviteKey
+ *
+ * @property string $value
+ * @property int $game_id
+ * @property int|null $user_id
+ * @property string $role
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Game $game
+ * @property-read \App\Models\User|null $user
+ * @method static \Database\Factories\InviteKeyFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|InviteKey newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|InviteKey newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|InviteKey query()
+ * @method static \Illuminate\Database\Eloquent\Builder|InviteKey whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|InviteKey whereGameId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|InviteKey whereRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|InviteKey whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|InviteKey whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|InviteKey whereValue($value)
+ * @mixin \Eloquent
+ */
 class InviteKey extends Model
 {
     use HasFactory;
