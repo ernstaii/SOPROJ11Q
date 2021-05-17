ffb8fa11e5dbc882bff0c4a15f866c0e5d25b6f4
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-05-17 11:08:48 +0200

    refactor(score-updated-event): Renamend properties

--- app/Events/ScoreUpdatedEvent.php
+++ app/Events/ScoreUpdatedEvent.php
@@ -4,14 +4,14 @@ namespace App\Events;
 
 class ScoreUpdatedEvent extends GameEvent
 {
-    public $thiefScore;
-    public $policeScore;
+    public $thief_score;
+    public $police_score;
 
     public function __construct($gameId, $policeScore, $thiefScore)
     {
         parent::__construct($gameId);
-        $this->policeScore = $policeScore;
-        $this->thiefScore = $thiefScore;
+        $this->police_score = $policeScore;
+        $this->thief_score = $thiefScore;
     }
 
     public function broadcastAs()
