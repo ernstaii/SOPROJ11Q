a1990750d43c54b1992ace8b94b17468a9d9c3c1
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-06-16 20:18:16 +0200

    refactor(thiefCaughtEvent): Removed logic of calling new interval event when thief has been caught, because event should be called every interval and not on a randomly moment

--- app/Events/ThiefCaughtEvent.php
+++ app/Events/ThiefCaughtEvent.php
@@ -20,59 +20,15 @@ class ThiefCaughtEvent extends GameEvent
 
     public function __construct(User $user)
     {
-        $this->smokescreened_users = new Collection();
-
         $this->user = $user;
         $game = $user->get_game();
-        $this->gameId = $game->id;
+        parent::__construct($game->id);
         $this->message = 'Boef ' . $user->username . ' is zojuist gevangen!';
 
         Notification::create([
             'game_id' => $game->id,
             'message' => $this->message
         ]);
-
-        $users = new Collection($game->get_users_filtered_on_last_verified());
-        $users = $this->check_active_smokescreens($users);
-        event(new GameIntervalEvent($game->id, $users, $game->loot, $this->drone_is_active($users), $game->time_left, $this->smokescreened_users));
-        $game->last_interval_at = Carbon::now();
-        $game->save();
-    }
-
-    private function drone_is_active($users)
-    {
-        $drone_activated = false;
-        foreach ($users as $user)
-            if ($user->gadgets()->count() > 0)
-                foreach ($user->gadgets() as $gadget)
-                    if ($gadget->pivot->in_use && $gadget->name === Gadgets::Drone) {
-                        $gadget->pivot->in_use = null;
-                        $gadget->pivot->activated_at = null;
-                        $gadget->pivot->save();
-                        $drone_activated = true;
-                    }
-
-        return $drone_activated;
-    }
-
-    private function check_active_smokescreens(Collection $users)
-    {
-        for ($i = 0; $i < $users->count(); $i++) {
-            if ($users[$i]->gadgets()->count() > 0) {
-                $removed_user_count = 0;
-                foreach ($users[$i]->gadgets() as $gadget)
-                    if ($gadget->pivot->in_use && $gadget->name === Gadgets::Smokescreen) {
-                        $gadget->pivot->in_use = null;
-                        $gadget->pivot->activated_at = null;
-                        $gadget->pivot->save();
-                        $this->smokescreened_users->push($users[$i]);
-                        $users->splice($i - $removed_user_count, 1);
-                        $removed_user_count += 1;
-                    }
-            }
-        }
-
-        return $users;
     }
 
     public function broadcastAs()
